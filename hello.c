693b1ab8cf898e1ded834fa06551b9e59cb81447
Micah739696 <mhakonson2@huskers.unl.edu>
2026-08-27 09:25:05 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Micah Hakonson
+ *         mhakonson2@huskers.unl.edu
+ * Date: 2026/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("I love the School of Computing!\n");
+  printf("Robotics Engineering.\n");
+  printf("Micah Hakonson.\n");
+
+  return 0;
+}
